1471ba72c2fa429984020af6fddd57bc7c875910
Romain GONÇALVES <romain.goncalves@gmail.com>
2019-05-21 13:49:38 +0200

    Adds logo

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,13 +7,13 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { StaticQuery, graphql } from "gatsby"
+import { StaticQuery, graphql, Link } from "gatsby"
 
 import "./layout.css"
 
 const Layout = ({ children }) => (
-	<StaticQuery
-		query={graphql`
+  <StaticQuery
+    query={graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -22,30 +22,47 @@ const Layout = ({ children }) => (
         }
       }
     `}
-		render={data => (
-			<>
-				<div
-					style={{
-						margin: `0 auto`,
-						maxWidth: 960,
-						padding: `0px 1.0875rem 1.45rem`,
-						paddingTop: 0,
-					}}
-				>
-					<main>{children}</main>
-					<footer>
-						© {new Date().getFullYear()}, Built with
-            {` `}
-						<a href="https://www.gatsbyjs.org">Gatsby</a>
-					</footer>
-				</div>
-			</>
-		)}
-	/>
+    render={data => (
+      <>
+        <div
+          style={{
+            margin: `0 auto`,
+            maxWidth: 960,
+            padding: `0px 1.0875rem 1.45rem`,
+            paddingTop: 0,
+          }}
+        >
+          <main>{children}</main>
+          <section id="footer">
+            {/* <ul class="icons">
+              <li><a href="#" class="icon alt fa-twitter"><span class="label">Twitter</span></a></li>
+              <li><a href="#" class="icon alt fa-facebook"><span class="label">Facebook</span></a></li>
+              <li><a href="#" class="icon alt fa-instagram"><span class="label">Instagram</span></a></li>
+              <li><a href="#" class="icon alt fa-github"><span class="label">GitHub</span></a></li>
+              <li><a href="#" class="icon alt fa-envelope"><span class="label">Email</span></a></li>
+            </ul> */}
+            <ul class="copyright">
+              <p className="lead">
+                Copyright &copy; 3rgo.tech {(new Date()).getFullYear() !== 2018 ? "2018 - " + (new Date()).getFullYear() : "2018"}
+                {` - `}
+                <Link to="/legal/">Mentions L&eacute;gales</Link>
+              </p>
+              <p className="font-weight-light">
+                <small>
+                  Built with <a href="https://gatsbyjs.org/" target="_blank" rel="noopener noreferrer">Gatsby</a>, <a href="https://reactjs.org/" target="_blank" rel="noopener noreferrer">React</a> and <a href="https://getbootstrap.com/" target="_blank" rel="noopener noreferrer">Bootstrap</a>.<br />
+                  Based on Photon theme by <a target="_blank" rel="noopener noreferrer" href="https://html5up.net/">HTML5 UP</a>
+                </small>
+              </p>
+            </ul>
+          </section>
+        </div>
+      </>
+    )}
+  />
 )
 
 Layout.propTypes = {
-	children: PropTypes.node.isRequired,
+  children: PropTypes.node.isRequired,
 }
 
 export default Layout
--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,20 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Accueil" />
+    <section id="header">
+      <div class="inner">
+        <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+          <Image />
+        </div>
+        <h1>Hi, I'm <strong>Photon</strong>, another fine<br />
+          little freebie from <a href="http://html5up.net">HTML5 UP</a>.</h1>
+        <p>Accumsan feugiat mi commodo erat lorem ipsum, sed magna<br />
+          lobortis feugiat sapien sed etiam volutpat accumsan.</p>
+        <ul class="actions special">
+          <li><a href="#one" class="button scrolly">Discover</a></li>
+        </ul>
+      </div>
+    </section>
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
