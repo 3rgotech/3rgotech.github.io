4633cd289510a30a7ee482df6e7dd903b122b2aa
Romain GONÇALVES <romain.goncalves@gmail.com>
2019-05-21 11:32:54 +0200

    Cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `3rgo.tech`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Romain GONÇALVES`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
