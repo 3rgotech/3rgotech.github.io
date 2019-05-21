83b06487c5b53e658f9da5c9a74f6b10ee5e0ea4
Romain GONÇALVES <romain.goncalves@gmail.com>
2019-05-21 11:35:07 +0200

    Removes header

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,12 +9,11 @@ import React from "react"
 import PropTypes from "prop-types"
 import { StaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
 
 const Layout = ({ children }) => (
-  <StaticQuery
-    query={graphql`
+	<StaticQuery
+		query={graphql`
       query SiteTitleQuery {
         site {
           siteMetadata {
@@ -23,31 +22,30 @@ const Layout = ({ children }) => (
         }
       }
     `}
-    render={data => (
-      <>
-        <Header siteTitle={data.site.siteMetadata.title} />
-        <div
-          style={{
-            margin: `0 auto`,
-            maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
-            paddingTop: 0,
-          }}
-        >
-          <main>{children}</main>
-          <footer>
-            © {new Date().getFullYear()}, Built with
+		render={data => (
+			<>
+				<div
+					style={{
+						margin: `0 auto`,
+						maxWidth: 960,
+						padding: `0px 1.0875rem 1.45rem`,
+						paddingTop: 0,
+					}}
+				>
+					<main>{children}</main>
+					<footer>
+						© {new Date().getFullYear()}, Built with
             {` `}
-            <a href="https://www.gatsbyjs.org">Gatsby</a>
-          </footer>
-        </div>
-      </>
-    )}
-  />
+						<a href="https://www.gatsbyjs.org">Gatsby</a>
+					</footer>
+				</div>
+			</>
+		)}
+	/>
 )
 
 Layout.propTypes = {
-  children: PropTypes.node.isRequired,
+	children: PropTypes.node.isRequired,
 }
 
 export default Layout
