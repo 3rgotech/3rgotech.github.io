043b21f2d9fbf8013fb6c4a80e39fcb301af9136
Romain GONÇALVES <romain.goncalves@gmail.com>
2020-02-19 20:39:39 +0100

    Adds description to gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `3rgo.tech`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    description: `3rgo.tech company website`,
     author: `Romain GONÇALVES`,
   },
   plugins: [
